f854ba228dd88acad83d729d781491bdca3b7bf9
David Brown <david.brown@linaro.org>
2016-05-27 16:29:23 -0600

    Add hikey flashing tool

---
+++ flash-hikey.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Assuming fastboot is in the path (TODO: Consider building this).
+# This will flash the parts of the image we are able to build, notably
+# the bootloader, secure image, and kernel/initrd.
+
+deploy=build-hikey/tmp-glibc/deploy/images/hikey-optee64
+
+fastboot flash -u bl1 $deploy/bl1.bin
+fastboot flash -u fastboot $deploy/fip.bin
+fastboot flash -u boot $deploy/optee-image-hikey-optee64.uefi
+
+fastboot reboot
